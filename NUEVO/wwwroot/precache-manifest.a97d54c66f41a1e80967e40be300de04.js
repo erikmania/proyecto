bba1f7855ef9f04209f3166f2510c20741166b92
erikmania <51883451+erikmania@users.noreply.github.com>
2019-06-17 00:56:36 +0200

    Add files via upload

---
+++ NUEVO/wwwroot/precache-manifest.a97d54c66f41a1e80967e40be300de04.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3793b9297a82ab828275",
+    "url": "/css/app.50abb0bb.css"
+  },
+  {
+    "revision": "3793b9297a82ab828275",
+    "url": "/js/app.ffdb4502.js"
+  },
+  {
+    "revision": "5422d5b67e3348336b74",
+    "url": "/css/chunk-vendors.4f7d6e88.css"
+  },
+  {
+    "revision": "5422d5b67e3348336b74",
+    "url": "/js/chunk-vendors.768d12cc.js"
+  },
+  {
+    "revision": "fc8875a2ae740e1fc7ae1107d2205406",
+    "url": "/img/tarjeta1.fc8875a2.png"
+  },
+  {
+    "revision": "d322b3e680ee0071e58d21381131820a",
+    "url": "/img/registro-home.d322b3e6.png"
+  },
+  {
+    "revision": "86aa5f04ebb2f529f832ee1e928620ad",
+    "url": "/img/c.86aa5f04.png"
+  },
+  {
+    "revision": "14baf1f06c54504c60ab3ba20bdcdc76",
+    "url": "/img/b.14baf1f0.png"
+  },
+  {
+    "revision": "d1f6ff182d6ce6fdd6b0bbb3ad94e7c4",
+    "url": "/img/tarjeta3.d1f6ff18.png"
+  },
+  {
+    "revision": "b2b2e1a48ed021bf32a7ec03b682c6ee",
+    "url": "/img/logo_fondo.b2b2e1a4.png"
+  },
+  {
+    "revision": "59b974bb008b3ed216a4c7d5e5151f65",
+    "url": "/img/a.59b974bb.png"
+  },
+  {
+    "revision": "a75b2f739de87b886f89df0a42f9e5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
